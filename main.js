414c33c3cc6120fde3a0baa3bec81baf0f5ce3f8
Taejune Youn <162322286+Designed-Realities@users.noreply.github.com>
2024-03-05 23:22:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function init() {
     scene = new THREE.Scene();
 
     new RGBELoader()
-        .setPath( 'https://github.com/Designed-Realities/Exhibition/raw/main/contents/models/' )
+        .setPath( '../main/contents/models/' )
         .load( 'studio_small_08_1k.hdr', function ( texture ) {
 
             texture.mapping = THREE.EquirectangularReflectionMapping;
@@ -34,7 +34,7 @@ function init() {
 
             // model
 
-            const loader = new GLTFLoader().setPath( 'https://github.com/Designed-Realities/Exhibition/raw/main/contents/models/studio_small_08_1k.hdr' );
+            const loader = new GLTFLoader().setPath( '../main/contents/models/' );
             loader.load( 'Test.glb', async function ( gltf ) {
                 const model = gltf.scene;
               
